00a143d780d5967c56b6b861f0f12de0ffe9b121
Penahli Ibrahim <penahliibrahim58@gmail.com>
2025-04-20 02:58:03 +0400

    API Route Updated

---
+++ app/api/blogs/[id]/route.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+import { createClient } from "@/utils/supabase/server";
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const supabase = await createClient();
+  const id = params.id;
+
+  if (!id) {
+    return NextResponse.json({ error: "ID is required" }, { status: 400 });
+  }
+
+  const { data, error } = await supabase.from("blogs").select("*").eq("id", id);
+
+  if (error) {
+    return NextResponse.json({ error: "Todo not found" }, { status: 404 });
+  }
+
+  return NextResponse.json(data);
+}
